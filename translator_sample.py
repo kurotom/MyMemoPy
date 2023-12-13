892862b4fce50a2be93759cc7003b492f1690452
kurotom <55354389+kurotom@users.noreply.github.com>
2023-12-13 12:31:55 -0300

    add example use file

---
+++ translator_sample.py
@@ -0,0 +1,29 @@
+'''
+Example of use, anonymous user and valid user.
+'''
+
+from mymemopy.translator import MyMemoryTranslate
+
+
+def valid_user():
+    vu = MyMemoryTranslate(email='example@example.com')
+    print(vu)
+    print(vu.get_quota())
+    res = vu.translate('hola', 'es', 'en')
+    print(res)
+    print(vu)
+
+def anon_user():
+    au = MyMemoryTranslate()
+    print(au)
+    print(au.get_quota())
+    res = au.translate('hola', 'es', 'en')
+    print(res)
+    print(au)
+
+
+
+if __name__ == '__main__':
+    anon_user()
+
+    valid_user()
